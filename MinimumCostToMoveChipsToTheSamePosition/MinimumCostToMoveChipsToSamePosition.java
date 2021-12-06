a68b1110350c6b4a775008d9763def3a74898b8f
5432roy <960289@gmail.com>
2021-12-06 01:12:18 -0800

    daily prob

---
+++ MinimumCostToMoveChipsToTheSamePosition/MinimumCostToMoveChipsToSamePosition.java
@@ -0,0 +1,15 @@
+package MinimumCostToMoveChipsToTheSamePosition;
+
+public class MinimumCostToMoveChipsToSamePosition {
+    public int minCostToMoveChips(int[] position) {
+        int odd = 0, even = 0;
+        
+        for(int i : position){
+            if(i % 2 == 0) even++;
+            else odd++;
+        }
+        
+        return Math.min(odd, even);
+    }
+    
+}
