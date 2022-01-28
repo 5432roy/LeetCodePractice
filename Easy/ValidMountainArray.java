3c8f695deb95c7199dbcb465a3d466dec478c630
5432roy <960289@gmail.com>
2022-01-27 17:17:06 -0800

    easy array question

---
+++ Easy/ValidMountainArray.java
@@ -0,0 +1,20 @@
+package Easy;
+
+public class ValidMountainArray {
+    public boolean validMountainArray(int[] arr) {
+        int index = 0;
+        if(arr.length < 3) return false;
+        while(index < arr.length - 1){
+            if(arr[index] < arr[index + 1]) index++;
+            else if(arr[index] == arr[index + 1]) return false;
+            else break;
+        }
+        if(index == arr.length - 1 || index == 0) return false;
+        while(index < arr.length - 1){
+            if(arr[index] > arr[index + 1]) index++;
+            else return false;
+        }
+        
+        return true;
+    }
+}
