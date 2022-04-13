622d1eef0437afd91b20b6939d29df23e634811c
Shang-Yu Chan <87119388+5432roy@users.noreply.github.com>
2022-04-12 21:18:30 -0700

    modify the variable to meaningful name

--- SpiralMatrix/SpiralMatrixII.java
+++ SpiralMatrix/SpiralMatrixII.java
@@ -1,23 +1,23 @@
 package SpiralMatrix;
 
 public class SpiralMatrixII {
-    
+
     public int[][] generateMatrix(int n) {
         int[][] res = new int[n][n];
         int[][] direction = new int[][]{{0,1,0,-1},{1,0,-1,0}};
-        int x = 0, y = 0, d = 0, count = n * n;
+        int row = 0, col = 0, d = 0, count = n * n;
         
         for(int i = 1; i <= count; i++){
-            res[x][y] = i;
-            d = turn(x + direction[0][d], y + direction[1][d], n, res) ? (d + 1) % 4 : d;
-            x += direction[0][d];
-            y += direction[1][d];
+            res[row][col] = i;
+            d = turn(row + direction[0][d], col + direction[1][d], n, res) ? (d + 1) % 4 : d;
+            row += direction[0][d];
+            col += direction[1][d];
         }
         
         return res;
     }
     
-    private boolean turn(int x, int y, int n, int[][] matrix){
-        return x < 0 || x >= n || y < 0 || y >= n || matrix[x][y] != 0;
+    private boolean turn(int row, int col, int n, int[][] matrix){
+        return row < 0 || row >= n || col < 0 || col >= n || matrix[row][col] != 0;
     }
 }
