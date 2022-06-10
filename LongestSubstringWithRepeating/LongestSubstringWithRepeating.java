e6c20f7df07046f94cc3d29053d6c5db2aa0e059
Shang-Yu Chan <87119388+5432roy@users.noreply.github.com>
2022-06-10 18:49:14 +0800

    daily prob

--- LongestSubstringWithRepeating/LongestSubstringWithRepeating.java
+++ LongestSubstringWithRepeating/LongestSubstringWithRepeating.java
@@ -3,7 +3,7 @@ package LongestSubstringWithRepeating;
 import java.util.HashMap;
 
 public class LongestSubstringWithRepeating {
-
+    /*
     public int lengthOfLongestSubstring(String s){
         int max = 0;
         for(int i = 0; i < s.length(); i++){
@@ -20,5 +20,18 @@ public class LongestSubstringWithRepeating {
             if(max == s.length() - i) return max;
         }
         return max;
+    }
+    */
+    public int lengthOfLongestSubstring(String s) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        int res = 0, mark = -1;
+        for(int i = 0; i < s.length(); i++) {
+            if(map.containsKey(s.charAt(i))) {
+                mark = Math.max(mark, map.get(s.charAt(i)));
+            }
+            res = Math.max(res, i - mark);
+            map.put(s.charAt(i), i);
+        }
+        return mark == -1 ? s.length() : res;
     }
 }
