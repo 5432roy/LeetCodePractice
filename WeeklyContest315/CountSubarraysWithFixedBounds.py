9e1c9501425756e8c4a3aabdd2a20a8613b0e8b4
Shang-Yu Chan <87119388+5432roy@users.noreply.github.com>
2022-10-20 00:46:08 -0700

    update new solution that bases on puring mathematic logic

--- WeeklyContest315/CountSubarraysWithFixedBounds.py
+++ WeeklyContest315/CountSubarraysWithFixedBounds.py
@@ -5,11 +5,25 @@ class Solution:
             if nums[i] == minK: min_i = i
             if nums[i] == maxK: max_i = i
             if nums[i] < minK or nums[i] > maxK: head = i
-            print(f"min: {min_i}, max: {max_i}, head: {head}")
             res += max(0, min(max_i, min_i) - head)
         return res
 
 
-s = Solution()
+class Solution2:
+    def countSubarrays(self, nums: list[int], minK: int, maxK: int) -> int:
+        def cnt(minK: int, maxK: int) -> int:
+            ans, curr = 0, 0
+            for num in nums:
+                if minK <= num <= maxK:
+                    curr += 1
+                    ans += curr
+                else:
+                    curr = 0
+            print(f"minK: {minK}, maxK: {maxK}, ans: {ans}")
+            return ans
+        
+        return cnt(minK, maxK) - cnt(minK, maxK - 1) - cnt(minK + 1, maxK) + cnt(minK + 1, maxK - 1)
+
+s = Solution2()
 print(s.countSubarrays(nums = [1,3,5,2,7,5], minK = 1, maxK = 5))
 print(s.countSubarrays(nums = [1,1,1,1], minK = 1, maxK = 1))
