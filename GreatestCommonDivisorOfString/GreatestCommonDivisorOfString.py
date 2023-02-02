68e2814c1caba10cba87fcd58c800a7901a2dad2
Shang-Yu Chan <960289@gmail.com>
2023-02-01 22:52:57 -0800

    daily prob

--- GreatestCommonDivisorOfString/GreatestCommonDivisorOfString.py
+++ GreatestCommonDivisorOfString/GreatestCommonDivisorOfString.py
@@ -1,3 +1,11 @@
+"""
+For two strings s and t, we say "t divides s" if and only if s = t + ... + t (i.e., t is concatenated with itself one or more times).
+
+Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+
+https://leetcode.com/problems/greatest-common-divisor-of-strings/
+"""
+
 def gcdOfStrings(self, str1: str, str2: str) -> str:
     if len(str2) > len(str1):
         str1, str2 = str2, str1
